0cd2fd4cd9f6b582fec77b7b30bde976312afb40
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2023-01-16 17:58:33 +0400

    v 0.0.2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 export default function HomePage() {
     return <>
-        <h1>Welcome, ПД121, to Next.js!</h1>
+        <h1>Welcome, ПД121, to Next.js!!!!!!</h1>
+        <hr/>
         <a href="/about">About</a>
     </>
 }
